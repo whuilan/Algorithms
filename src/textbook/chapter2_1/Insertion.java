4b5e00086945c77ab8ce46f3b2c0f4b5241c8804
whuilan <whuilan26@gmail.com>
2019-06-04 13:21:53 +0800

    selection and insertion

---
+++ src/textbook/chapter2_1/Insertion.java
@@ -0,0 +1,42 @@
+package textbook.chapter2_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Insertion {
+   public static void sort(Comparable[] a){
+       int N = a.length;
+       for(int i=1;i<N;i++){
+           for(int j=i;j>0&&less(a[j],a[j-1]);j--){
+               exch(a,j,j-1);
+           }
+       }
+   }
+   public static boolean less(Comparable v,Comparable w){
+       return v.compareTo(w)<0;
+   }
+   public static void exch(Comparable[] a,int i,int j){
+       Comparable t = a[i];
+       a[i] = a[j];
+       a[j] = t;
+   }
+   public static void show(Comparable[] a){
+       for(int i=0;i<a.length;i++){
+           StdOut.print(a[i]+" ");
+       }
+       StdOut.println();
+   }
+   public static boolean isSorted(Comparable[] a){
+       for(int i=1;i<a.length;i++){
+           if(less(a[i],a[i-1])) return false;
+       }
+       return true;
+   }
+    public static void main(String[] args){
+        //In in = new In();
+        // String[] b = {"S","O","R","T","E","X","A","M","P","L","E"};
+        Integer[] b = {1,3,5,2,4};
+        sort(b);
+        assert isSorted(b);
+        show(b);
+    }
+}
---
+++ src/textbook/chapter2_1/SortCompare.java
@@ -0,0 +1,41 @@
+package textbook.chapter2_1;
+
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import textbook.chapter1_4.chapter1_4_2.Stopwatch;
+
+public class SortCompare {
+    public static double time(String alg, Comparable[] a){
+        Stopwatch timer = new Stopwatch();
+        if(alg.equals("Insertion")) Insertion.sort(a);
+        if(alg.equals("Selection")) Selection.sort(a);
+//        if(alg.equals("Shell")) Shell.sort(a);
+//        if(alg.equals("Merge")) Merge.sort(a);
+//        if(alg.equals("Quick")) Quick.sort(a);
+//        if(alg.equals("Heap")) Heap.sort(a);
+        return timer.elapsedTime();
+    }
+    public static double timeRandomInput(String alg,int N,int T){
+        // 使用算法alg将T个长度为N的数组排序，得到总时间
+        double total = 0.0;
+        Double[] a = new Double[N];
+        for(int t=0;t<T;t++){
+            for(int i=0;i<N;i++){
+                a[i] = StdRandom.uniform();
+            }
+            total += time(alg,a);
+        }
+        return total;
+    }
+    public static void main(String[] args){
+        String alg1 = args[0];
+        String alg2 = args[1];
+        int N = Integer.parseInt(args[2]);
+        int T = Integer.parseInt(args[3]);
+        double t1 = timeRandomInput(alg1,N,T);
+        double t2 = timeRandomInput(alg2,N,T);
+        StdOut.printf("For %d random Doubles\n %s is ",N,alg1);
+        StdOut.printf("%.1f times faster than %s\n",t2/t1,alg2);
+    }
+}
