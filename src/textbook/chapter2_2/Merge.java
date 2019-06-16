8db76630b41dfb1d4c924305d36297c4c001cf3f
whuilan <whuilan26@gmail.com>
2019-06-16 17:19:16 +0800

    add Merge sort

---
+++ src/textbook/chapter2_2/Merge.java
@@ -0,0 +1,44 @@
+package textbook.chapter2_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Merge {
+    private static Comparable[] aux;
+    public static void sort(Comparable[] a){
+        aux = new Comparable[a.length];
+        sort(a,0,a.length-1);
+    }
+    public static void sort(Comparable[] a,int lo,int hi){
+        if(hi<=lo) return;
+        int mid = (lo+hi)/2;
+        sort(a,lo,mid);
+        sort(a,mid+1,hi);
+        merge(a,lo,mid,hi);
+    }
+    public static  void merge(Comparable[] a,int lo,int mid,int hi){
+        int i = lo,j = mid+1;
+        for(int k=lo;k<=hi;k++){
+            aux[k] = a[k];
+        }
+        for(int k=lo;k<=hi;k++){
+            if(i>mid) a[k] = aux[j++];
+            else if(j>hi) a[k] = aux[i++];
+            else if(less(aux[j],aux[i])) a[k]=aux[j++];
+            else a[k] = aux[i++];
+        }
+    }
+    public static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    public static void show(Comparable[] a){
+        for(int i=0;i<a.length;i++){
+            StdOut.print(a[i]+" ");
+        }
+        StdOut.println();
+    }
+    public static void main(String[] args){
+        Integer[] b = {6,4,2,3,1,5,7,0};
+        sort(b);
+        show(b);
+    }
+}
