9d6e4db52358d54ed629348c81d65ac79bff7e70
whuilan <whuilan26@gmail.com>
2019-09-03 22:53:47 +0800

    add delete() and keys()

--- src/textbook/chapter3_2/BST.java
+++ src/textbook/chapter3_2/BST.java
@@ -1,5 +1,9 @@
 package textbook.chapter3_2;
 
+import textbook.chapter1_3_3.Queue;
+
+import java.util.NoSuchElementException;
+
 public class BST<Key extends Comparable<Key>,Value> {
 
     private Node root;
@@ -36,6 +40,7 @@ public class BST<Key extends Comparable<Key>,Value> {
     private Value get(Node x,Key key){
         // 在以x为结点的子树中查找并返回key对应的值
         // 如果找不到则返回null
+        if (key == null) throw new IllegalArgumentException("calls get() with a null key");
         if(x==null)
             return null;
         int cmp = key.compareTo(x.key);
@@ -63,14 +68,16 @@ public class BST<Key extends Comparable<Key>,Value> {
         return x;
     }
     public Key min(){
+        if (isEmpty()) throw new NoSuchElementException("calls min() with empty symbol table");
         return min(root).key;
     }
     private Node min(Node x){
-        if(x.left == null) //默认链表非空
+        if(x.left == null)
             return x;
         return min(x.left);
     }
     public Key max(){
+        if (isEmpty()) throw new NoSuchElementException("calls min() with empty symbol table");
         return max(root).key;
     }
     private Node max(Node x){
@@ -80,6 +87,8 @@ public class BST<Key extends Comparable<Key>,Value> {
     }
     // 向上取整，返回小于等于key的最大键
     public Key floor(Key key){
+        if (key == null) throw new IllegalArgumentException("argument to floor() is null");
+        if (isEmpty()) throw new NoSuchElementException("calls floor() with empty symbol table");
         Node x = floor(root,key);
         // 注意二叉树中不一定有比给定key小的键，如果所有键都比给定的key大，则应返回null
         if(x == null)
@@ -102,6 +111,8 @@ public class BST<Key extends Comparable<Key>,Value> {
     }
     // 向下取整，返回大于等于key的最小建
     public Key ceiling(Key key){
+        if (key == null) throw new IllegalArgumentException("argument to floor() is null");
+        if (isEmpty()) throw new NoSuchElementException("calls floor() with empty symbol table");
         Node x = ceiling(root,key);
         if(x==null)
             return null;
@@ -126,6 +137,7 @@ public class BST<Key extends Comparable<Key>,Value> {
         if (k < 0 || k >= size()) {
             throw new IllegalArgumentException("called select() with invalid argument: " + k);
         }
+        if (isEmpty()) throw new NoSuchElementException("calls floor() with empty symbol table");
         return select(root,k).key;
     }
     private Node select(Node x,int k){
@@ -155,7 +167,9 @@ public class BST<Key extends Comparable<Key>,Value> {
         else
             return rank(x.right,key)+t+1;
     }
-    public void deleteMin(){
+    public void deleteMin()
+    {
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
         root = deleteMin(root);
     }
     private Node deleteMin(Node x){
@@ -178,8 +192,7 @@ public class BST<Key extends Comparable<Key>,Value> {
         return x;
     }
     public void delete(Key key){
-//        if(!contains(key))
-//            return;
+        //  默认key和二叉树非空
         root = delete(root,key);
     }
     private Node delete(Node x,Key key){
@@ -191,8 +204,36 @@ public class BST<Key extends Comparable<Key>,Value> {
         else if(cmp>0)
              x.right = delete(x.right,key);
         else{
-
+            if(x.left==null)
+                return x.right;
+            if(x.right==null)
+                return x.left;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
         }
-
+        x.N = size(x.left)+size(x.right)+1;
+        return x;
+    }
+    public Iterable<Key> keys(){
+        return keys(min(),max());
+    }
+    public Iterable<Key> keys(Key lo,Key hi){
+        Queue<Key> queue = new Queue<>();
+        keys(root,lo,hi,queue);
+        return queue;
+    }
+    private void keys(Node x,Key lo,Key hi,Queue<Key> queue){
+        if(x==null)
+            return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = lo.compareTo(x.key);
+        if(cmplo<0)
+            keys(x.left,lo,hi,queue);
+        if(cmplo<=0 && cmphi>=0)
+            queue.enqueue(x.key);
+        if(cmphi>0)
+            keys(x.right,lo,hi,queue);
     }
 }
