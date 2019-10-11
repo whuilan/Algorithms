e7d80b041a1e2667119dcd195ef0808b854c7939
whuilan <whuilan26@gmail.com>
2019-10-11 22:54:32 +0800

    add Cycle

---
+++ src/textbook/chapter4_1/Cycle.java
@@ -0,0 +1,42 @@
+package textbook.chapter4_1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * 使用深度优先搜索来检测环：给定的图是无环图吗？
+ */
+public class Cycle {
+    private boolean[]  marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph g){
+        marked = new boolean[g.V()];
+        for(int s = 0; s < g.V(); s++){
+            if(!marked[s]){
+                dfs(g, s, s);
+            }
+        }
+    }
+    private void dfs(Graph g, int v, int u){
+        marked[v] = true;
+        for(int w : g.adj(v)){
+            if(!marked[w]){
+                dfs(g, w, v);
+            }
+            else if(w != u){
+                hasCycle = true;
+                return;
+            }
+        }
+    }
+    public boolean hasCycle(){
+        return hasCycle;
+    }
+
+    public static void main(String[] args){
+        Graph g = new Graph(new In(args[0]));
+        Cycle cycle = new Cycle(g);
+        StdOut.println(cycle.hasCycle);
+    }
+}
