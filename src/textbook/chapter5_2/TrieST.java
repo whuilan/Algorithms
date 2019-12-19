c44ec06c5ef397c9551943f42eb9c165d4f499e9
whuilan <whuilan26@gmail.com>
2019-12-19 23:03:09 +0800

    add collect and keysWithPrefix

--- src/textbook/chapter5_2/TrieST.java
+++ src/textbook/chapter5_2/TrieST.java
@@ -2,6 +2,7 @@ package textbook.chapter5_2;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import textbook.chapter1_3_3.Queue;
 
 /**
  * 基于单词查找树的符号表
@@ -50,6 +51,48 @@ public class TrieST<Value> {
         x.next[c] = put(x.next[c], key, val, d + 1);
         return x; // 没想到
     }
+    // 键值对的数量
+    public int size(){
+        return size(root);
+    }
+    // 单词查找树的延时递归方法size()，即遍历单词查找树的所有结点并记录非空值结点的总数(实际使用时不推荐这种方式，
+    // 因为会给用例造成性能上的问题)
+    private int size(Node x){
+        if(x == null){
+            return 0;
+        }
+        int cnt = 0;
+        if(x.val != null){
+            cnt++;
+        }
+        for(char c = 0; c < R; c++){  // 注意此处，会将字符c转换成相应的编码再跟R比较
+            cnt += size(x.next[c]);   // 这里写的时候只写了右半部分，那每次递归调用size()的时候都会把cnt清零！
+        }
+        return cnt;
+    }
+    // 符号表中的所有键
+    public Iterable<String> keys(){
+        return keysWithPrefix("");
+    }
+    // 所有以pre为前缀的键
+    public Iterable<String> keysWithPrefix(String pre){
+        Queue<String> q = new Queue<>();
+        Node childTrie = get(root, pre, 0);
+        collect(childTrie, pre, q);
+        return q;
+    }
+    // 递归地访问结点来找到并显式地表示键(pre为从根路径到结点x上的所有字符)
+    private void collect(Node x, String pre, Queue<String> q){
+        if(x == null){
+            return;
+        }
+        if(x.val != null){
+            q.enqueue(pre);
+        }
+        for(char c = 0; c < R; c++){
+            collect(x.next[c], pre + c, q);
+        }
+    }
 
     public static void main(String[] args){
         TrieST<Integer> st = new TrieST<>();
@@ -57,8 +100,14 @@ public class TrieST<Value> {
             String key = StdIn.readString();
             st.put(key, i);
         }
-        String key = "by";
-        int val = st.get(key);
-        StdOut.println(val);
+        for(String key : st.keysWithPrefix("se")){
+            StdOut.println(key);
+        }
+        StdOut.println();
+        for(String key : st.keys()){
+            StdOut.println(key);
+        }
+//        int size = st.size();
+//        StdOut.println(size);
     }
 }
