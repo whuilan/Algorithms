4c1f55b113d75a46885bdecc38ca5451167f62f6
whuilan <whuilan26@gmail.com>
2020-02-02 18:45:57 +0800

    add sword_to_offer Problem 3(_2)

---
+++ src/sword_to_offer/Problem3.java
@@ -0,0 +1,54 @@
+package sword_to_offer;
+
+/**
+ * 面试题3，数组中重复的数字
+ */
+public class Problem3 {
+    // 法一：暴力比较，时间复杂度为N^2，另外少了对非法输入的检测
+//    public boolean duplicate(int numbers[],int length,int [] duplication) {
+//        for(int i = 0; i < length; i++){
+//            for(int j = i + 1; j < length; j++){
+//                if(numbers[i] == numbers[j]){
+//                    duplication[0] = numbers[i];
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+    // 法二：将值为i的元素调整到第i个位置上,时间复杂度为O(N),空间复杂度为O(1)，加入对非法输入的检测
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if(numbers == null || length < 0){
+            return false;
+        }
+        for(int i = 0;i < length;i++){
+            if(numbers[i] < 0 || numbers[i] > length -1){
+                return false;
+            }
+        }
+        for(int i = 0; i < length; i++){
+            while(numbers[i] != i){
+                if(numbers[i] == numbers[numbers[i]]){
+                    // numbers[i]同时出现在索引为i和处，重复
+                    duplication[0] = numbers[i];
+                    return true;
+                }
+                // 交换i和numbers[i]位置处的值
+                int temp = numbers[i];
+                numbers[i] = numbers[temp];
+                numbers[temp] = temp;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int[] numbers = {2, 3, 1, 0, 2, 5};
+        int length = numbers.length;
+        int[] duplication = new int[1];
+        Problem3 ex = new Problem3();
+        boolean isFound = ex.duplicate(numbers, length, duplication);
+        System.out.println(isFound);
+    }
+}
---
+++ src/sword_to_offer/Problem3_2.java
@@ -0,0 +1,53 @@
+package sword_to_offer;
+
+/**
+ * 不修改数组找出重复的数字,时间复杂度为:O(nlogn), 空间复杂度为O(1)，不能找出所有的重复数字
+ */
+public class Problem3_2 {
+    public static int getDuplication(int[] nums, int length){
+        if(nums == null || length <= 0){
+            return -1;
+        }
+        int start = 1, end = length - 1; // 注意start初始值，题目P41中说了数组元素均在1~length-1之间
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+            int count = countRange(nums, length, start, mid);
+            // 注意循环到最后的截止情况
+            if(start == mid){
+                if(count > 1){
+                    return start;
+                }
+                else {
+                    break;
+                }
+            }
+            if(count > (mid - start + 1)){
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int countRange(int[] nums, int length, int start, int end){
+        if(nums == null || length <= 0){
+            return -1;
+        }
+        int count = 0;
+        for(int i = 0;i < length;i++){
+            if(nums[i] >= start && nums[i] <= end){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {6, 5, 4, 3, 2, 6, 2};
+        int length = nums.length;
+        int count = getDuplication(nums, length);
+        System.out.println(count);
+    }
+}
