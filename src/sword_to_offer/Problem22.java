f9525eb1e509a9ede7e84258e78fec6292efd249
whuilan <whuilan26@gmail.com>
2020-02-21 12:08:33 +0800

    add Problem 22 FindKthToTail & 23 EntryNodeOfLoop

---
+++ src/sword_to_offer/Problem22.java
@@ -0,0 +1,56 @@
+package sword_to_offer;
+
+import java.util.List;
+
+/**
+ * P134 链表中倒数第k个节点,核心：1使用两个指针来遍历一次链表 2鲁棒性，注意k的各种取值范围
+ * 假设链表有n个节点，那么倒数第k个节点其实就是从头数第(n-k+1)个节点，所以要使第一个指针
+ * 先走k-1步，从第k步开始两个指针一起走，那么第一个指针走到尾节点n时，第二个指针刚好走
+ * 到第(n-k+1)个节点
+ */
+public class Problem22 {
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode FindKthToTail(ListNode head,int k) {
+        if (head == null || k <= 0){
+            return null;
+        }
+        // 第一个指针先走k-1步
+        ListNode pointerOne = head;
+        for (int i = 1; i <= k - 1; i++){
+            if (pointerOne.next != null){  // 至少这k-1步还没走到尾节点
+                pointerOne = pointerOne.next;
+            }else {
+                return null;   // 说明链表中的节点整数小于k
+            }
+        }
+        // 从第k步开始，第二个指针也从头开始遍历
+        ListNode pointerTwo = head;
+        while (pointerOne.next != null){   // 第一个指针到达尾节点后应该终止循环
+            pointerOne = pointerOne.next;
+            pointerTwo = pointerTwo.next;
+        }
+        return pointerTwo;
+    }
+
+    public static void main(String[] args){
+        ListNode first = new ListNode(1);
+        ListNode node_2 = new ListNode(2);
+        ListNode node_3 = new ListNode(3);
+        ListNode node_4 = new ListNode(3);
+        // ListNode node_5 = new ListNode(4);
+        first.next = node_2;
+        node_2.next = node_3;
+        node_3.next = node_4;
+        Problem22 problem22 = new Problem22();
+        ListNode KthToTail = problem22.FindKthToTail(first, 5);
+        System.out.println("finish");
+    }
+}
---
+++ src/sword_to_offer/Problem23.java
@@ -0,0 +1,58 @@
+package sword_to_offer;
+
+/**
+ * P139链表中环的入口节点，同样要用两个指针P1、P2
+ * 首先要判断链表中是否含有环，此时两个指针都从首节点开始遍历，但是速度不同P1次走一步，
+ * P2一次走两步。若链表中含有环，那么两个指针一定会在环中的某个节点相遇。此时再让P1从
+ * 头开始遍历，并且速度变为一次一步，同时P2从相遇点开始遍历，P1、P2再次相遇的地方即为
+ * 环的入口节点
+ */
+public class Problem23 {
+    public static class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    public ListNode EntryNodeOfLoop(ListNode pHead)
+    {
+        if (pHead == null || pHead.next == null){  // 至少含有两个节点，不然一定没有环
+            return null;
+        }
+        ListNode p1 = pHead, p2 = pHead;
+        // 找出p1和p2相遇的节点，注意要用do..while循环，不然刚开始p1,p2相等进不去while循环
+        do {
+            p1 = p1.next.next;
+            p2 = p2.next;
+        } while (p1 != null && p1.next != null && p1 != p2);
+        if (p1 != p2){
+            return null; // 说明链表中没有环
+        }
+        p1 = pHead;
+        while (p1 != p2){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
+    }
+
+    public static void main(String[] args){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node3;
+        Problem23 problem23 = new Problem23();
+        ListNode entryNode = problem23.EntryNodeOfLoop(node1);
+        System.out.println();
+    }
+}
